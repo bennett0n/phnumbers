619ab51182a4c8c5ba9eac958981e7f78c8ff252
Bennett Manalo <bennett@MacBen.local>
2020-04-28 14:30:20 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const prefixlst = require('./jsondb/prefix.json');
+
+
+app.use(morgan('short'));
+
+app.get("/checkproviderph/:number", (req, res)=>{
+  
+  var input = req.params.number;
+  var prefix = input.slice(0,4);
+
+  console.log(`Input: ${input}`);
+
+  var result;
+  try{
+  result = prefixlst[prefix].provider;
+  }catch(err){}
+
+  if(result)
+    res.send(`Provider: ${prefixlst[prefix].provider}`);
+  else
+    res.send("Prefix not found or incorrect format. Follow 11 digit PH Mobile number 09XXXXXXXXX.")
+
+  
+
+});
+
+app.listen(80, () => {
+  console.log("Server is up listening at port 80");
+});
